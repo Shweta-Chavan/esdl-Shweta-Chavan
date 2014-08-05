7bde31b8a278a873d972a339741027d6543e8e5c
Shweta-Chavan <shweta.chavan@cumminscollege.in>
2014-08-05 08:57:11 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,18 @@
 #include<iostream>
-using namespace std;
-int main()
+using namespace std;void main()
 {
  cout<<"hello"<<"\n";
  return 0;
 
+}
+void add()
+{
+ int a,b,c;
+ cout<<"enter the no:"<<"\n";
+ cin>>a;
+ cout<<"enter the no:"<<"\n";
+ cin>>b;
+ c=a+b;
+ cout<<"the answer:";
+ cout<<c;
 }
